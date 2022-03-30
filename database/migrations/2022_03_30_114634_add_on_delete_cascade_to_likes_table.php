40872c412553faedfdaa2194b121ecf3f1b25cc6
anemadhon <anemadhon@gmail.com>
2022-03-30 12:33:54 +0700

    add on delete cascade

---
+++ database/migrations/2022_03_30_114634_add_on_delete_cascade_to_likes_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOnDeleteCascadeToLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('likes', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+            $table->dropForeign(['thread_id']);
+            $table->foreign('thread_id')
+                    ->references('id')
+                    ->on('threads')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('likes', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users');
+            $table->dropForeign(['thread_id']);
+            $table->foreign('thread_id')
+                    ->references('id')
+                    ->on('threads');
+        });
+    }
+}
